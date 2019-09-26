c9cbf04c82544deb1e02e5e47a98d8d797a61cff
nick <zudo@doki.space>
2019-09-26 12:19:37 +0100

    Add more functionality to update settings (a proper "check for updates" button and a latest version indicator)

--- src/js/components/tabs/options/UpdateOptions.vue
+++ src/js/components/tabs/options/UpdateOptions.vue
@@ -4,27 +4,68 @@
         <p>{{_("renderer.tab_options.section_updates.subtitle")}}</p>
         <br>
         <p><strong>{{_("renderer.tab_options.section_updates.description_current_version", version)}}</strong></p>
+        <template v-if="!error">
+            <p><strong>{{_("renderer.tab_options.section_updates.description_latest_version", latest)}}</strong></p>
+            <br>
+            <p v-if="has_update">{{_("renderer.tab_options.section_updates.description_has_update")}}</p>
+            <p v-else>{{_("renderer.tab_options.section_updates.description_no_update")}}</p>
+        </template>
+        <p v-else>{{_("renderer.tab_options.section_updates.description_error")}}</p>
         <br>
-        <p><button @click="checkUpdates" class="primary"><i class="fas fa-sync-alt fa-fw"></i> {{_("renderer.tab_options.section_updates.button_check")}}</button></p>
+        <p v-if="has_update && !error">
+            <button @click="doUpdate" class="primary" :disabled="checking">
+                <i class="fas fa-download fa-fw"></i> {{_("renderer.tab_options.section_updates.button_update")}}
+            </button>
+        </p>
+        <p v-else>
+            <button @click="checkUpdate" class="primary" :disabled="checking">
+                <i class="fas fa-sync-alt fa-fw"></i> {{_("renderer.tab_options.section_updates.button_check")}}
+            </button>
+        </p>
     </div>
 </template>
 
 <script>
     import Logger from "../../../utils/Logger";
+    import * as semver from "semver";
 
     export default {
         name: "UpdateOptions",
         methods: {
             _: ddmm.translate,
-            checkUpdates() {
+            doUpdate() {
                 Logger.info("Updates", "Forcing update check");
                 ddmm.app.update();
+                this.checking = true;
+            },
+            checkUpdate() {
+                this.checking = true;
+                fetch("https://api.github.com/repos/DokiDokiModManager/Mod-Manager/releases/latest")
+                    .then(res => res.json()).then(release => {
+                    this.error = false;
+                    this.latest = release.name;
+                }).catch(() => {
+                    this.error = true;
+                }).finally(() => {
+                    this.checking = false;
+                });
             }
         },
-        data () {
+        data() {
             return {
-                version: ddmm.version
+                version: ddmm.version,
+                latest: "",
+                error: false,
+                checking: true
             }
+        },
+        computed: {
+            has_update() {
+                return this.latest && semver.gt(this.latest, this.version);
+            }
+        },
+        mounted() {
+            this.checkUpdate();
         }
     }
 </script>
