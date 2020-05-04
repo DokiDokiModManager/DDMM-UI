5f2c7a2832c35c1a11b52292ff353ad05d197422
Nick Booth <zudo@doki.space>
2020-05-04 15:04:34 +0100

    Mod search and strings

--- src/components/tabs/downloads/StoreSection.vue
+++ src/components/tabs/downloads/StoreSection.vue
@@ -10,28 +10,36 @@
 
         <template v-else-if="error">
             <br>
-            <p><i class="fas fa-exclamation-triangle"></i> Can't load the mods right now.</p>
+            <p><i class="fas fa-exclamation-triangle"></i> {{_("renderer.tab_downloads.store.error_loading")}}</p>
         </template>
 
-        <br>
+        <template v-else>
 
+            <br>
 
-        <div v-for="mod in mods" class="mod" @click="viewMod(mod)" style="cursor: pointer;">
-            <div class="image">
-                <img alt="" :src="mod.icon" width="75" v-if="mod.icon">
-                <img alt="" src="../../../images/logo.png" width="75" v-else>
+            <p><input type="text" v-model="search" :placeholder="_('renderer.tab_downloads.store.placeholder_search')"></p>
+
+            <br>
+
+            <div v-for="mod in modList" class="mod" @click="viewMod(mod)" style="cursor: pointer;">
+                <div class="image">
+                    <img alt="" :src="mod.icon" width="75" v-if="mod.icon">
+                    <img alt="" src="../../../images/logo.png" width="75" v-else>
+                </div>
+                <div>
+                    <h3><strong>{{mod.name}}</strong></h3>
+                    <p>{{mod.shortDescription}}</p>
+                </div>
             </div>
-            <div>
-                <h3><strong>{{mod.name}}</strong></h3>
-                <p>{{mod.shortDescription}}</p>
-            </div>
-        </div>
+        </template>
     </div>
 </template>
 
 <script>
     import DDLCModClub from "../../../js/stores/DDLCModClub";
     import Logger from "../../../js/utils/Logger";
+
+    import Fuse from "fuse.js";
 
     export default {
         name: "StoreSection",
@@ -40,6 +48,8 @@
                 modStore: new DDLCModClub(),
                 loaded: false,
                 error: false,
+                fuse: null,
+                search: "",
                 mods: []
             }
         },
@@ -49,10 +59,19 @@
                 this.$store.commit("preview_mod", mod);
             }
         },
+        computed: {
+            modList() {
+              if (!this.search) return this.mods;
+              return this.fuse.search(this.search).map(r => r.item);
+            }
+        },
         mounted() {
             this.modStore.getListing(0).then(listing => {
                 this.mods = listing.mods;
                 this.loaded = true;
+                this.fuse = new Fuse(this.mods, {
+                    keys: ["name", "description", "shortDescription"]
+                });
             }).catch(err => {
                 Logger.error("Mod Store", err.toString());
                 this.loaded = true;
