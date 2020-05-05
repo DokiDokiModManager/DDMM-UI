ef2325dceae3a4c21c1803f767b87c3623d75adb
Nick Booth <zudo@doki.space>
2020-05-05 20:35:31 +0100

    Add mod search

--- src/components/tabs/downloads/DownloadedDownloadsSection.vue
+++ src/components/tabs/downloads/DownloadedDownloadsSection.vue
@@ -2,7 +2,11 @@
     <div>
         <h1>{{_("renderer.tab_downloads.downloaded.title")}}</h1>
 
-        <div v-for="mod in mods" class="mod">
+        <br>
+
+        <p><input type="text" v-model="search" :placeholder="_('renderer.tab_downloads.store.placeholder_search')"></p>
+
+        <div v-for="mod in modList" class="mod">
             <div>
                 <h3>{{getDisplayName(mod)}}</h3>
                 <p>{{getPathToMod(mod)}}</p>
@@ -22,6 +26,8 @@
 </template>
 
 <script>
+    import Fuse from "fuse.js";
+
     export default {
         name: "DownloadedDownloadsSection",
         methods: {
@@ -45,10 +51,23 @@
                 });
             }
         },
+        data() {
+            return {
+                fuse: null,
+                search: ""
+            }
+        },
         computed: {
             mods() {
                 return this.$store.state.game_data.mods;
+            },
+            modList() {
+                if (!this.search) return this.mods;
+                return this.fuse.search(this.search).map(r => r.item);
             }
+        },
+        mounted() {
+            this.fuse = new Fuse(this.mods);
         }
     }
 </script>
