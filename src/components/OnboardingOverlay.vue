473095076f62b88524ceaa475c12143c7e2a59c8
Nick Booth <zudo@doki.space>
2020-05-11 21:40:51 +0100

    Fully implement a rough version of the onboarding UI

--- src/components/OnboardingOverlay.vue
+++ src/components/OnboardingOverlay.vue
@@ -16,14 +16,14 @@
                 </div>
             </div>
 
-            <div class="wizard-step" v-if="step === 2">
+            <div class="wizard-step" v-else-if="step === 2">
                 <div class="wizard-step-content">
                     <h1>First time setup</h1>
-                    <p><strong>Please read the following instructions carefully.</strong></p>
+                    <p>Please read the following instructions carefully.</p>
 
                     <br>
 
-                    <div class="regular-font">
+                    <div>
                         <div>
                             <h2>Download DDLC</h2>
                             <p>Visit the DDLC website, click Download Now and follow the instructions. When you
@@ -63,11 +63,11 @@
                 </div>
             </div>
 
-            <div class="wizard-step" v-if="step === 3">
+            <div class="wizard-step" v-else-if="step === 3">
                 <div class="wizard-step-content">
                     <div>
                         <h1>First time setup</h1>
-                        <p><strong>Please read the following instructions carefully.</strong></p>
+                        <p>Please read the following instructions carefully.</p>
                     </div>
 
                     <br>
@@ -84,44 +84,90 @@
 
                     <br>
 
-                    <div class="regular-font">
-                        <div v-if="selection.validating">
-                            <i class="fas fa-spinner fa-spin"></i> Please wait a moment...
-                        </div>
+                    <div v-if="selection.validating">
+                        <i class="fas fa-spinner fa-spin"></i> Please wait a moment...
+                    </div>
 
-                        <div v-if="selection.directory">
-                            <h2><i class="fas fa-exclamation-triangle"></i> You selected a folder instead of the DDLC
-                                zip file</h2>
-                            <p>Please make sure you aren't selecting an extracted copy of the game and try again.</p>
-                        </div>
+                    <div v-if="selection.directory">
+                        <h2><i class="fas fa-exclamation-triangle"></i> You selected a folder instead of the DDLC
+                            zip file</h2>
+                        <p>Please make sure you aren't selecting an extracted copy of the game and try again.</p>
+                    </div>
 
-                        <template v-if="selection.validated">
-                            <div v-if="selection.valid">
-                                <div v-if="!selection.version_match">
-                                    <h2><i class="fas fa-exclamation-triangle"></i> That doesn't look like the right zip file</h2>
-                                    <p>Please go back and double-check you followed the instructions exactly as shown.</p>
-                                </div>
-                                <div v-else>
-                                    <h2><i class="fas fa-check"></i> We found your copy of DDLC</h2>
-                                    <p>You're now ready to move on to the next step!</p>
-                                </div>
+                    <template v-if="selection.validated">
+                        <div v-if="selection.valid">
+                            <div v-if="!selection.version_match">
+                                <h2><i class="fas fa-exclamation-triangle"></i> That doesn't look like the right zip
+                                    file</h2>
+                                <p>Please go back and double-check you followed the instructions exactly as shown.</p>
                             </div>
                             <div v-else>
-                                <h2><i class="fas fa-exclamation-triangle"></i> That doesn't look like the right file</h2>
-                                <p>Please double-check you dragged the right file and try again.</p>
+                                <h2><i class="fas fa-check"></i> We found your copy of DDLC</h2>
+                                <p>You're now ready to move on to the next step!</p>
                             </div>
-                        </template>
-                    </div>
+                        </div>
+                        <div v-else>
+                            <h2><i class="fas fa-exclamation-triangle"></i> That doesn't look like the right file</h2>
+                            <p>Please double-check you dragged the right file and try again.</p>
+                        </div>
+                    </template>
 
                 </div>
                 <div class="wizard-step-controls">
                     <button class="secondary" @click="previous"><i class="fas fa-arrow-left fa-fw"></i> Previous
                     </button>
                     <button class="primary"
-                            @click="$emit('close')"
+                            @click="next"
                             :disabled="!selection.valid || !selection.version_match">
                         <i class="fas fa-arrow-right fa-fw"></i> Next
                     </button>
+                </div>
+            </div>
+
+            <div class="wizard-step" v-else-if="step === 4">
+                <div class="wizard-step-content">
+                    <div>
+                        <h1>First time setup</h1>
+                        <p>Just a couple more things...</p>
+                    </div>
+
+                    <br>
+
+                    <h2>Save Location</h2>
+                    <p>If you want to store your mods on a different drive, now's your chance to choose it.</p>
+
+                    <br>
+
+                    <p><label for="ob_install_folder">Save Location</label></p>
+
+                    <div class="input-row">
+                        <input type="text" v-model="save_directory" readonly id="ob_install_folder">
+                        <button class="primary">Change</button>
+                    </div>
+
+                    <br>
+
+                    <h2>Who's your favourite character?</h2>
+                    <p>Don't think, just answer.</p>
+
+                    <br>
+
+                    <p><label for="ob_waifu">Best Girl</label></p>
+
+                    <p>
+                        <select v-model="background" id="ob_waifu">
+                            <option value="default.png">All of them!</option>
+                            <option value="x-base-monika.png">Monika!</option>
+                            <option value="x-base-natsuki.png">Natsuki!</option>
+                            <option value="x-base-sayori.png">Sayori!</option>
+                            <option value="x-base-yuri.png">Yuri!</option>
+                        </select>
+                    </p>
+                </div>
+                <div class="wizard-step-controls">
+                    <button class="secondary" @click="previous"><i class="fas fa-arrow-left fa-fw"></i> Previous
+                    </button>
+                    <button class="primary" @click="finalise"><i class="fas fa-check fa-fw"></i> Finish</button>
                 </div>
             </div>
         </div>
@@ -132,6 +178,7 @@
 
     import Link from "./elements/Link";
     import DropZone from "./elements/DropZone";
+    import Logger from "../js/utils/Logger";
 
     export default {
         name: "OnboardingOverlay",
@@ -151,7 +198,9 @@
                     validated: false,
                     valid: false,
                     version_match: false
-                }
+                },
+                save_directory: ddmm.config.readConfigValue("installFolder"),
+                background: "default.png"
             }
         },
         mounted() {
@@ -173,14 +222,16 @@
                 this.selection.validated = false;
                 this.selection.directory = true;
             },
-            fileCheck(item) {
-                this.selection.path = item.path;
-                console.log("Validating", this.path);
+            validate(path) {
+                this.selection.path = path;
                 this.selection.directory = false;
                 this.selection.valid = false;
                 this.selection.validating = true;
                 this.selection.validated = false;
                 ddmm.onboarding.validateGame(this.selection.path);
+            },
+            fileCheck(item) {
+                this.validate(item.path);
             },
             browse() {
                 ddmm.onboarding.browseForGame();
@@ -192,6 +243,13 @@
                     this.selection.version_match = result.version_match;
                     this.selection.valid = result.success;
                 }
+            },
+            finalise() {
+                Logger.info("Onboarding", "Finalising setup!");
+                this.$store.commit("set_background", this.background);
+                ddmm.config.saveConfigValue("installFolder", this.save_directory);
+                this.$emit("close");
+                ddmm.onboarding.finalise(this.selection.path);
             }
         }
     }
