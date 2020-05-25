3c09e47eb7762367dc018b68312bfb9c952e7788
Nick Booth <zudo@doki.space>
2020-05-25 20:28:42 +0100

    Move issue report text to language files.

--- src/components/dialogs/IssueReportDialog.vue
+++ src/components/dialogs/IssueReportDialog.vue
@@ -1,40 +1,33 @@
 <template>
     <Dialog>
-        <h2>Report an issue</h2>
+        <h2>{{_("renderer.modal_issue_report.title")}}</h2>
         <br>
-        <p>Sorry to hear you're having an issue. Please tell me what the issue is, in as much detail as
-            possible.</p>
+        <p>{{_("renderer.modal_issue_report.description")}}</p>
         <br>
-        <p>Not all reports will receive a reply. Please use English if you can so I have the best chance of
-            understanding your issue.</p>
-        <br>
-        <label for="issue_report_type">Issue Type</label>
+        <label for="issue_report_type">{{_("renderer.modal_issue_report.label_type")}}</label>
         <p><select id="issue_report_type" v-model="type">
-            <option value="bug">Application bug</option>
-            <option value="broken mod">Mod not working</option>
-            <option value="other">Other</option>
+            <option value="bug">{{_("renderer.modal_issue_report.option_bug")}}</option>
+            <option value="broken mod">{{_("renderer.modal_issue_report.option_broken_mod")}}</option>
         </select></p>
-        <br>
-        <label for="issue_report_contact">Contact Details</label>
-        <p><input type="text" id="issue_report_contact" v-model="contact"></p>
         <template v-if="type === 'broken mod'">
             <br>
-            <label for="issue_report_mod">Link to mod</label>
-            <p><input type="text" id="issue_report_mod" v-model="mod"></p>
+            <label for="issue_report_mod">{{_("renderer.modal_issue_report.label_mod")}}</label>
+            <p><input type="text" id="issue_report_mod" v-model="mod" :placeholder="_('renderer.modal_issue_report.placeholder_mod')"></p>
         </template>
         <br>
-        <label for="issue_report_body">Message</label>
+        <label for="issue_report_body">{{_("renderer.modal_issue_report.label_body")}}</label>
         <p><textarea style="resize: none;"
                      id="issue_report_body"
                      rows="5"
+                     :placeholder="_('renderer.modal_issue_report.placeholder_body')"
                      v-model="body"></textarea></p>
         <br>
         <p>
             <button class="primary" :disabled="sending" @click="sendAndClose">
                 <i class="fas fa-paper-plane fa-fw"></i>
-                Send Report
+                {{_("renderer.modal_issue_report.button_submit")}}
             </button>
-            <button class="secondary" @click="close"><i class="fas fa-times fa-fw"></i> Cancel</button>
+            <button class="secondary" @click="close"><i class="fas fa-times fa-fw"></i> {{_("renderer.modal_issue_report.button_cancel")}}</button>
         </p>
     </Dialog>
 </template>
@@ -42,20 +35,21 @@
 <script>
 
     import Dialog from "./base/Dialog";
+    import Link from "../elements/Link";
 
     export default {
         name: "IssueReportDialog",
-        components: {Dialog},
+        components: {Link, Dialog},
         data() {
             return {
                 type: "bug",
                 body: "",
                 mod: "",
-                contact: "",
                 sending: false
             }
         },
         methods: {
+            _: ddmm.translate,
             close() {
                 this.$store.commit("hide_modal", {modal: "issue_report"});
             },
@@ -68,7 +62,6 @@
                     },
                     body: JSON.stringify({
                         type: this.type,
-                        contact: this.contact,
                         body: this.body,
                         mod: this.type === "broken mod" ? this.mod : null
                     })
