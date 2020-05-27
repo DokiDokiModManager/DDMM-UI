6274f94d43cf4b316205affc477eeee04c9875b7
Nick Booth <zudo@doki.space>
2020-05-27 16:17:35 +0100

    Fix launch issue (this was really stupid)

--- src/js/utils/Launcher.js
+++ src/js/utils/Launcher.js
@@ -5,7 +5,7 @@ export default class Launcher {
     static launch(install, store) {
         Logger.info("Game Launch", "Preparing to launch install " + install);
         if (!install.archived) {
-            ddmm.mods.launchInstall(install);
+            ddmm.mods.launchInstall(install.folderName);
             Logger.info("Game Launch", "Launched " + install);
         } else {
             store.commit("select_install", {install});
